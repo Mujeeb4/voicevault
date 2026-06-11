1adb11b1f596b497eceb47fae572328cc9024ee8
Mujeeb Rathore <mujeebrathore4@gmail.com>
2026-06-11 20:39:10 +0500

    added for production files

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -eu
+
+ROLE="${1:-web}"
+
+run_migrations() {
+  if [ "${RUN_MIGRATIONS:-true}" = "true" ]; then
+    python manage.py migrate --noinput
+  fi
+}
+
+case "$ROLE" in
+  web)
+    run_migrations
+    exec supervisord -c /app/supervisord.conf
+    ;;
+  celery-worker)
+    run_migrations
+    exec celery -A config worker --loglevel="${CELERY_LOG_LEVEL:-info}" -Q "${CELERY_QUEUES:-default,transcription,voice,analysis}" --concurrency="${CELERY_CONCURRENCY:-2}"
+    ;;
+  celery-beat)
+    run_migrations
+    exec celery -A config beat --loglevel="${CELERY_LOG_LEVEL:-info}"
+    ;;
+  *)
+    exec "$@"
+    ;;
+esac
